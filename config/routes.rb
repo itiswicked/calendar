bd31bf4b7abbf6a09b8eda2c7680472477ae0e96
Nathan <nathanrbourke@gmail.com>
2016-01-18 18:12:09 -0500

    fix route issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'events#index'
 
   devise_for :users, controllers: { omniauth_callbacks: "callbacks" }
-  devise_scope :user { root 'devise/sessions#new' }
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 
   resources :events, only: [:index]
 end
