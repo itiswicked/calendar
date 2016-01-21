aeb0afb91b75e18f8a170d93e2ca725c6a86a888
Nathan <nathanrbourke@gmail.com>
2016-01-20 21:51:37 -0500

    add current day highlight

--- app/models/month.rb
+++ app/models/month.rb
@@ -7,21 +7,23 @@ class Month
 
   def create!
     @data = (1..num_of_days)
-      .to_a
       .map { |n| { date: Time.new(@year, @month, n), in_month: true } }
-    prepend_month
-    append_month
-    @data.flatten!
-    self
+      .map { |day| today?(day) }
+      .unshift(prepend_month)
+      .push(append_month)
+      .flatten
   end
 
   def last_month
-    t = time_obj - 1.month
-    Month.new(t.year, t.month)
+    jump_month(:-)
   end
 
   def next_month
-    t = time_obj + 1.month
+    jump_month(:+)
+  end
+
+  def jump_month(operator)
+    t = time_obj.send(operator, 1.month)
     Month.new(t.year, t.month)
   end
 
@@ -31,6 +33,15 @@ class Month
 
   def name
     time_obj.strftime("%B")
+  end
+
+  def today?(day)
+    if Time.at(day[:date]).to_date === Time.at(Time.now).to_date
+      day[:today] = true
+    else
+      day[:today] = false
+    end
+    day
   end
 
   private
@@ -48,7 +59,7 @@ class Month
     end
 
     def prepend_month
-      days = (1..last_month.num_of_days)
+      (1..last_month.num_of_days)
         .to_a
         .last(weekday_of_first)
         .map do |n|
@@ -57,11 +68,10 @@ class Month
             in_month: false
           }
         end
-      @data.unshift(days)
     end
 
     def append_month
-      days = (1..next_month.num_of_days)
+      (1..next_month.num_of_days)
         .to_a
         .first(6 - weekday_of_last)
         .map do |n|
@@ -70,6 +80,5 @@ class Month
             in_month: false
           }
         end
-      @data << days
     end
 end
