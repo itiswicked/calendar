1b2649e4bffda1cf9096e436694971cd1aa1247a
Nathan <nathanrbourke@gmail.com>
2016-01-21 14:29:41 -0500

    finish feature

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -2,7 +2,7 @@ class MonthsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @month = Month.new(2016, 1)
+    @month = Month.new(month_params)
     @month.create!
     first_date = @month.data.first[:date].strftime("%Y-%m-%d")
     last_date = @month.data.last[:date].strftime("%Y-%m-%d")
@@ -16,5 +16,11 @@ class MonthsController < ApplicationController
 
   def date_has_event?(date, events)
     events.select { |event| event.start_time.day == date.day }.any?
+  end
+
+
+  def month_params
+    params.permit(:year, :month)
+    # { year: params[:year], month: params[:month] }
   end
 end
