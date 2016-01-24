030f0427a9f5e92f6c59aea2b469fbc575c01dc3
Nathan <nathanrbourke@gmail.com>
2016-01-23 21:05:15 -0500

    remove unused partial, remove unused methods from resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,21 +1,4 @@
 class Resource < ActiveRecord::Base
   belongs_to :event
-  belongs_to :inventory_item
-
-  def is_lighting?
-  end
-
-  def is_sound?
-
-  end
-
-  def is_scenic?
-
-  end
-
-  def is_catering?
-
-  end
-
-  
+  belongs_to :inventory_item  
 end
