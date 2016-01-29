713fc731f8351399693949f01a8b9a48a8e0ff4b
Nathan <nathanrbourke@gmail.com>
2016-01-29 13:19:11 -0500

    finish first pass of styling signup page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,11 @@
 Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.precompile += %w( months.css )
-Rails.application.config.assets.precompile += %w( devise/sessions.css )
+Rails.application.config.assets.precompile += %w(
+  months.css
+  users/registrations.css
+  devise/sessions.css
+)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
