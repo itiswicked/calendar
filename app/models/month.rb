bd7f5fc48b0dd4d5965bd7bcc1b053cbd8a081e5
Nathan <nathanrbourke@gmail.com>
2016-02-10 11:22:49 -0500

    first attempt at refactoring class

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,14 +1,13 @@
 class Month
-  attr_reader :month, :year, :data
+  attr_reader :year, :month, :data
   def initialize(info_hash)
-    @month = (info_hash[:month] || Time.now.month).to_i
     @year = (info_hash[:year] || Time.now.year).to_i
+    @month = (info_hash[:month] || Time.now.month).to_i
   end
 
   def create!
     @data = (1..num_of_days)
-      .map { |n| { date: Time.new(@year, @month, n), in_month: true } }
-      .map { |day| today?(day) }
+      .map { |day| create_day(@year, @month, day) }
       .unshift(prepend_month)
       .push(append_month)
       .flatten
@@ -30,23 +29,15 @@ class Month
     time_obj.strftime("%B")
   end
 
-  def today?(day)
-    if Time.at(day[:date]).to_date === Time.at(Time.now).to_date
-      day[:today] = true
-    else
-      day[:today] = false
-    end
-    day
-  end
 
   private
 
-    def weekday_of_first
-      time_obj.wday
-    end
-
-    def weekday_of_last
-      Time.new(@year, @month, num_of_days).wday
+    def create_day(year, month, date)
+      day = {}
+      day[:date] = Time.new(year, month, date)
+      day[:in_month] = in_month? day[:date]
+      day[:today] = is_today? day[:date]
+      day
     end
 
     def time_obj
@@ -58,24 +49,33 @@ class Month
       Month.new(year: t.year, month: t.month)
     end
 
+    def weekday_of_first
+      time_obj.wday
+    end
+
+    def weekday_of_last
+      Time.new(@year, @month, num_of_days).wday
+    end
+
     def prepend_month
       (1..last_m.num_of_days)
         .to_a
         .last(weekday_of_first)
-        .map do |n|
-          {
-            date: Time.new(last_m.year, last_m.month, n),
-            in_month: false
-          }
-        end
+        .map { |n| create_day(last_m.year, last_m.month, n) }
     end
 
     def append_month
       (1..next_m.num_of_days)
         .to_a
         .first(6 - weekday_of_last)
-        .map do |n|
-          { date: Time.new(next_m.year, next_m.month, n), in_month: false }
-        end
+        .map  { |n| create_day(next_m.year, next_m.month, n) }
+    end
+
+    def is_today?(date)
+      Time.at(date).to_date === Time.at(Time.now).to_date
+    end
+
+    def in_month?(date)
+      @month == date.month && @year == date.year
     end
 end
